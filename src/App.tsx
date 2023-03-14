21936c722a5653c34dc22a08e74519d6c6aa0750
Fikri Bill Gufran <billgufran@gmail.com>
2023-03-14 10:34:28 +0700

    works, alpha-vite-config.1

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import "./App.css";
 
 import {
   Box,
+  createTheme,
   type ColorMode,
   ThemeProvider,
 } from "@brikl/design-storefront";
-import CustomBox from "./Components/CustomBox"
+import CustomBox from "./Components/CustomBox";
+
+const theme = createTheme();
 
 function App() {
   const foo: ColorMode = "dark";
 
+  console.log({ theme });
+
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
